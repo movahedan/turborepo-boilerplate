bef59f40c7f7ac7fc8e92a66f145fd7e73529313
soheilmovahedan <soheil.movahedan.ext@luminus.be>
2024-04-21 00:46:00 +0200

    chore: install @next/bundle-analyzer to next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ["@repo/ui"],
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
 };
+
+const enableBundleAnalyzer =
+  process.env.BUNDLE_ANALYZER === "true" &&
+  process.env.NODE_ENV === "production";
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: enableBundleAnalyzer,
+  analyzerMode: "json",
+});
+
+module.exports = enableBundleAnalyzer
+  ? withBundleAnalyzer(nextConfig)
+  : nextConfig;
