28dae1993774300d09419b67884362fa0995d4b7
soheilmovahedan <soheil.movahedan.ext@luminus.be>
2024-04-21 00:39:28 +0200

    chore: remove docs applciation

--- apps/docs/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["../../packages/config-eslint/next.eslintrc.js"],
-  parserOptions: {
-    project: true,
-  },
-};
