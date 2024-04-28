0593be405d8f7f81f0af806f8769d3ce9ce378ca
soheilmovahedan <soheil.movahedan.ext@luminus.be>
2024-04-28 07:23:58 +0200

    feat: lint-staged filtered by affected files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,33 @@
+const rootDirAbsolutePath = process.cwd();
+
+function getRelativeFiles(files) {
+  return files.map((file) => file.replace(rootDirAbsolutePath, '.'));
+}
+
+function getTurboAffectedFilters(files) {
+  const affected = new Set();
+
+  files.forEach((file) => {
+    const [, , segment] = file.split('/');
+
+    affected.add(`@repo/${segment}`);
+  });
+
+  const affectedKeys = Array.from(affected.keys());
+  const affectedFilters = affectedKeys.map((key) => `--filter=${key}`);
+
+  return affectedFilters;
+}
+
 module.exports = {
-  "**/*.ts?(x)": () => ["yarn type-check", `yarn lint --quiet --fix`],
+  '*.{ts,tsx,js,json,html,css,scss,less,sass,svg}': (files) => {
+    const affectedFilters = getTurboAffectedFilters(files);
+
+    return [`npm run lint ${affectedFilters.join(' ')}`];
+  },
+  '**/*.{t,j}s?(x)': (files) => {
+    const affectedFilters = getTurboAffectedFilters(files);
+
+    return ['npm run type-check', `npm run test ${affectedFilters.join(' ')}`];
+  },
 };
